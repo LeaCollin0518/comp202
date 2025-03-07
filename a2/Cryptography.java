712fac69538e00584a239afd3c1f90ceac8c65c5
Lea Collin <leac@spotify.com>
2025-03-06 21:58:00 -0500

    finish a2

--- a2/Cryptography.java
+++ a2/Cryptography.java
@@ -1,15 +1,69 @@
 package a2;
 
+import java.util.Random;
+
 public class Cryptography {
   public static void main(String[] args) {
-    System.out.println(caesarEncrypt("zzz!", 29));
-    System.out.println(caesarDecrypt("aaa!", 29));
-    System.out.println(caesarEncrypt("go Rangers!", 3));
-    System.out.println(caesarDecrypt("jr Udqjhuv!", 3));
+
+    System.out.println(crackCipher("jr Udqjhuv!", 10));
+    System.out.println(permuteEncrypt("hello world!"));
+    System.out.println(permuteEncrypt("HEllo World!"));
+
+  }
+
+  public static void shuffle(char[] letters){
+    Random generator = new Random(12345);
+    double number_of_swaps = Math.pow(letters.length,4);
+
+    for (int i = 0; i < number_of_swaps; i++) {
+      int index_1 = generator.nextInt(letters.length);
+      int index_2 = generator.nextInt(letters.length);
+
+      char helper_swap = letters[index_1];
+      letters[index_1] = letters[index_2];
+      letters[index_2] = helper_swap;
+    }
+  }
+
+  public static char[] generatePermutation(){
+    char [] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+    shuffle(alphabet);
+
+    return alphabet;
+  }
+
+  public static String permuteEncrypt(String input){
+    char [] encryption_array = generatePermutation();
+
+    StringBuilder encryptedOutput = new StringBuilder();
+
+    for (int i = 0; i < input.length(); i++){
+      char currentChar = input.charAt(i);
+      char encrypted_char;
+
+      if(Character.toUpperCase(currentChar) >= 'A' && Character.toUpperCase(currentChar) <= 'Z'){
+        int encrypted_index = (int)Character.toUpperCase(currentChar) - (int)'A';
+        encrypted_char = encryption_array[encrypted_index];
+
+        if(Character.isLowerCase(currentChar)){
+          encrypted_char = Character.toLowerCase(encrypted_char);
+        }
+
+      }
+      else {
+        encrypted_char = currentChar;
+      }
+
+      encryptedOutput.append(encrypted_char);
+
+    }
+
+    return encryptedOutput.toString();
   }
 
   public static String caesarEncrypt(String originalMessage, int shift) {
-    String encryptedMessage = "";
+    StringBuilder encryptedMessage = new StringBuilder();
     shift = shift % 26;
 
     for (int i = 0; i < originalMessage.length(); i++) {
@@ -21,13 +75,12 @@ public class Cryptography {
             || (newChar > 'Z' && Character.isUpperCase(currentChar))) {
           newChar = (char) (currentChar - (26 - shift));
         }
-      }
-      else{
+      } else {
         newChar = currentChar;
       }
-      encryptedMessage += newChar;
+      encryptedMessage.append(newChar);
     }
-    return encryptedMessage;
+    return encryptedMessage.toString();
   }
 
   public static String caesarDecrypt(String encryptedMessage, int shift) {
